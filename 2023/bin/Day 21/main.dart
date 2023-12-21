cb0ea1738b3ba77f9b2c5ef65f16b1ab025f8414
victor.divi <victor.divi@upc.edu>
2023-12-21 09:48:02 +0100

    Do Day 21 Part 1

--- 2023/bin/Day 21/main.dart
+++ 2023/bin/Day 21/main.dart
@@ -1,1 +1,52 @@
-void main() {}
+import 'dart:collection';
+import 'dart:io';
+
+import '../common.dart';
+
+void main() {
+  final input = File('./bin/Day 21/input.txt')
+      .readAsLinesSync()
+      .map((e) => e.split(''))
+      .toList();
+  final start = getStartingPosition(input);
+
+  print(getPlotsReachableInSteps(input, 64, start));
+}
+
+Position getStartingPosition(List<List<String>> map) {
+  for (final (i, row) in map.indexed) {
+    for (final (j, cell) in row.indexed) {
+      if (cell == 'S') return (i, j);
+    }
+  }
+  throw "Starting Position not found";
+}
+
+int getPlotsReachableInSteps(List<List<String>> map, int steps, Position start,
+    [bool endless = false]) {
+  final even = <Position>{};
+  final odd = <Position>{};
+  final nextPositions = Queue<(Position, bool)>.from([(start, true)]);
+  final futurePositions = Queue<(Position, bool)>();
+  for (int i = 0; i <= steps; ++i) {
+    while (nextPositions.isNotEmpty) {
+      final (current, isEven) = nextPositions.removeFirst();
+      if ((!endless && current.isOutOfBounds(map)) ||
+          map[current.row % map.length]
+                  [current.col % map[current.row % map.length].length] ==
+              '#') {
+        continue;
+      }
+      final set = isEven ? even : odd;
+      if (set.contains(current)) continue;
+      set.add(current);
+      futurePositions.addAll(current.neighbors4
+          .map((e) => (e, !isEven))
+          .zippedWhere(
+              (pos, eve) => eve ? !even.contains(pos) : !odd.contains(pos)));
+    }
+    nextPositions.addAll(futurePositions);
+    futurePositions.clear();
+  }
+  return steps.isEven ? even.length : odd.length;
+}
