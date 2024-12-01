69975a4164b8e14ff82d369329db6219e0d5c815
Diviloper <vdiviloper@gmail.com>
2024-12-01 12:17:56 +0100

    Unswap Day 1 and 2

--- 2024/bin/Day 1/main.dart
+++ 2024/bin/Day 1/main.dart
@@ -5,5 +5,31 @@ import 'package:collection/collection.dart';
 import '../common.dart';
 
 void main() {
-  final lines = File('./input.txt').readAsLinesSync();
+  final (left, right) = File('./input.txt')
+      .readAsLinesSync()
+      .map((e) => e.split("   ").map(int.parse).toList())
+      .map((e) => (e.first, e.last))
+      .unzip();
+
+  first(left.copy(), right.copy());
+  second(left.copy(), right.copy());
+}
+
+void first(List<int> left, List<int> right) {
+  left.sort();
+  right.sort();
+
+  int totalDistance = IterableZip([left, right])
+      .map((e) => (e[1] - e[0]).abs())
+      .fold(0, (total, element) => total + element);
+
+  print(totalDistance);
+}
+
+void second(List<int> left, List<int> right) {
+  final counter = right.count;
+  int totalCount =
+      left.fold(0, (total, element) => total + element * counter[element]);
+
+  print(totalCount);
 }
