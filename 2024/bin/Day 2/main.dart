a0184f0e972e955cba2c8a0cf620be64ff194287
Diviloper <vdiviloper@gmail.com>
2024-12-02 06:59:31 +0100

    Finish Day 2

--- 2024/bin/Day 2/main.dart
+++ 2024/bin/Day 2/main.dart
@@ -1,9 +1,36 @@
 import 'dart:io';
 
-import 'package:collection/collection.dart';
-
 import '../common.dart';
 
 void main() {
-  final lines = File('./input.txt').readAsLinesSync();
+  final lines = File('./input.txt')
+      .readAsLinesSync()
+      .map((line) => line.split(' ').map(int.parse).toList())
+      .toList();
+
+  print(lines.where(isValid).length);
+  print(lines.where(isValidDampened).length);
+}
+
+bool isValid(List<int> line) {
+  final differences =
+      line.indexed.skip(1).map((e) => e.$2 - line[e.$1 - 1]).toList();
+  if (!differences.every((element) => element > 0) &&
+      !differences.every((element) => element < 0)) {
+    return false;
+  }
+
+  if (differences.any((element) => element.abs() < 1 || element.abs() > 3)) {
+    return false;
+  }
+  return true;
+}
+
+bool isValidDampened(List<int> line) {
+  if (isValid(line)) return true;
+  for (int i = 0; i < line.length; ++i) {
+    final copy = line.copy()..removeAt(i);
+    if (isValid(copy)) return true;
+  }
+  return false;
 }
