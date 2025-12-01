ebd2861d5db24dcd0d3f66dade6efc50fb11d949
Diviloper <vdiviloper@gmail.com>
2025-12-01 06:26:41 +0100

    Day 1

--- 2025/bin/Day 1/main.dart
+++ 2025/bin/Day 1/main.dart
@@ -7,6 +7,49 @@ void main() {
   second(input);
 }
 
-void first(List<String> input) {}
+void first(List<String> input) {
+  int value = 50;
+  int timesAtZero = 0;
+  for (final line in input) {
+    final direction = line.substring(0, 1);
+    final number = int.parse(line.substring(1));
+    if (direction == "L") {
+      value -= number;
+    } else {
+      value += number;
+    }
+    value = value % 100;
+    if (value == 0) {
+      timesAtZero++;
+    }
+  }
+  print(timesAtZero);
+}
 
-void second(List<String> input) {}
+void second(List<String> input) {
+  int previous = 50;
+  int value = 50;
+  int timesAtZero = 0;
+  for (final line in input) {
+    final direction = line.substring(0, 1);
+    final number = int.parse(line.substring(1));
+
+    final rotations = number ~/ 100;
+    timesAtZero += rotations;
+
+    if (direction == "L") {
+      value -= number % 100;
+    } else {
+      value += number % 100;
+    }
+
+    if (previous > 0 && value < 0) timesAtZero++;
+    if (value > 100) timesAtZero++;
+    value = value % 100;
+    if (value == 0) {
+      timesAtZero++;
+    }
+    previous = value;
+  }
+  print(timesAtZero);
+}
