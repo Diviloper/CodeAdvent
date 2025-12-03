1395f40d67fd3b7c5ad82a0e695a4e08259cb294
Diviloper <vdiviloper@gmail.com>
2025-12-03 06:27:15 +0100

    Day 3

--- 2025/bin/Day 3/main.dart
+++ 2025/bin/Day 3/main.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:collection/collection.dart';
+
 void main() {
   final input = File('./input.txt').readAsLinesSync();
 
@@ -7,6 +9,35 @@ void main() {
   second(input);
 }
 
-void first(List<String> input) {}
+void first(List<String> input) {
+  print(input.map(findMax).sum);
+}
 
-void second(List<String> input) {}
+int findMax(String bank) {
+  for (int i = 9; i >= 1; --i) {
+    for (int j = 9; j >= 1; --j) {
+      if (bank.contains(RegExp(".*$i.*$j.*"))) {
+        return i * 10 + j;
+      }
+    }
+  }
+  return 11;
+}
+
+void second(List<String> input) {
+  print(input.map(findMaxOverride).sum);
+}
+
+int findMaxOverride(String bank) {
+  int digits = 12;
+  final bankDigits = bank.split('').map(int.parse).toList();
+  int number = 0;
+  int start = 0;
+  while (digits > 0) {
+    final nextDigit = bankDigits.sublist(start, bank.length - digits + 1).max;
+    start = bankDigits.indexOf(nextDigit, start) + 1;
+    number = number * 10 + nextDigit;
+    digits--;
+  }
+  return number;
+}
