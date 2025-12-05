1e8746849aec69b8534ad44039e096b2665febdd
Diviloper <vdiviloper@gmail.com>
2025-12-05 06:30:27 +0100

    Day 5

--- 2025/bin/Day 5/main.dart
+++ 2025/bin/Day 5/main.dart
@@ -1,12 +1,64 @@
 import 'dart:io';
+import 'dart:math';
+import 'package:collection/collection.dart';
+
+typedef Range = (int, int);
 
 void main() {
-  final input = File('./input.txt').readAsLinesSync();
-
-  first(input);
-  second(input);
+  final (db, ingredients) = readInput();
+  first(db, ingredients);
+  second(db);
 }
 
-void first(List<String> input) {}
+(List<Range>, List<int>) readInput() {
+  final [dbInput, ingredientInput] = File('./input.txt')
+      .readAsStringSync()
+      .split('${Platform.lineTerminator}${Platform.lineTerminator}');
+  final db = dbInput
+      .split(Platform.lineTerminator)
+      .map((rs) => rs.split("-").map(int.parse).toList())
+      .map((l) => (l.first, l.last))
+      .toList();
+  final ingredients = ingredientInput
+      .split(Platform.lineTerminator)
+      .map(int.parse)
+      .toList();
+  return (db, ingredients);
+}
 
-void second(List<String> input) {}
+void first(List<Range> db, List<int> ingredients) {
+  final fresh = ingredients
+      .where((ing) => db.any((range) => ing >= range.$1 && ing <= range.$2))
+      .length;
+  print(fresh);
+}
+
+void second(List<Range> db) {
+  int previousDB;
+  do {
+    previousDB = db.length;
+    db = collapseDB(db);
+  } while (db.length != previousDB);
+  int freshIngredients = db.map((r) => r.$2 - r.$1 + 1).sum;
+  print(freshIngredients);
+}
+
+List<Range> collapseDB(List<Range> db) {
+  final newDB = <Range>[];
+  for (int i = 0; i < db.length; ++i) {
+    Range range = db[i];
+    for (int j = i + 1; j < db.length; ++j) {
+      if (overlap(db[i], db[j])) range = merge(range, db.removeAt(j--));
+    }
+    newDB.add(range);
+  }
+  return newDB;
+}
+
+bool overlap(Range first, Range second) {
+  return !(first.$2 < second.$1 || second.$2 < first.$1);
+}
+
+Range merge(Range first, Range second) {
+  return (min(first.$1, second.$1), max(first.$2, second.$2));
+}
