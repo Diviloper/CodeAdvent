f7203b17d0a29672814ccde8be66c1c6e012ec84
Diviloper <vdiviloper@gmail.com>
2025-12-11 06:25:42 +0100

    Day 11

--- 2025/bin/Day 11/main.dart
+++ 2025/bin/Day 11/main.dart
@@ -2,11 +2,56 @@ import 'dart:io';
 
 void main() {
   final input = File('./input.txt').readAsLinesSync();
-
-  first(input);
-  second(input);
+  final graph = parseGraph(input);
+  first(graph);
+  second(graph);
 }
 
-void first(List<String> input) {}
+Map<String, List<String>> parseGraph(List<String> input) {
+  final graph = <String, List<String>>{};
+  for (final line in input) {
+    final [key, values] = line.split(": ");
+    graph[key] = values.split(" ");
+  }
+  return graph;
+}
 
-void second(List<String> input) {}
+void first(Map<String, List<String>> graph) {
+  final next = <String, int>{"you": 1};
+  int paths = 0;
+  while (next.isNotEmpty) {
+    final current = next.keys.first;
+    final numPaths = next.remove(current)!;
+    if (current == "out") {
+      paths += numPaths;
+      continue;
+    }
+    for (final connection in graph[current]!) {
+      next.update(connection, (v) => v + numPaths, ifAbsent: () => numPaths);
+    }
+  }
+  print(paths);
+}
+
+void second(Map<String, List<String>> graph) {
+  final next = <(String, bool, bool), int>{("svr", false, false): 1};
+  int paths = 0;
+  while (next.isNotEmpty) {
+    final (current, dac, fft) = next.keys.first;
+    final numPaths = next.remove((current, dac, fft))!;
+    if (current == "out") {
+      if (dac && fft) paths += numPaths;
+      continue;
+    }
+    for (final connection in graph[current]!) {
+      final newDac = dac || connection == "dac";
+      final newFft = fft || connection == "fft";
+      next.update(
+        (connection, newDac, newFft),
+        (v) => v + numPaths,
+        ifAbsent: () => numPaths,
+      );
+    }
+  }
+  print(paths);
+}
